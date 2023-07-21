15f897af492f1e129ec7a5eb6d2251ae9367781c
Capsonchik <Serega00790@mail.ru>
2023-07-21 16:48:28 +0300

    Подготовка моков для новостей

---
+++ src/mocks.js
@@ -0,0 +1,32 @@
+export const topNews = {
+  img: '/img/news/topNews.png',
+  date: '',
+  text: 'Esports Group teams up with the Indianapolis Colts'
+}
+
+ export const newsList = [
+  {
+    id: 1,
+    img: '/img/news/news1.png',
+    date: '',
+    text: 'NAVI reveals s1mple fifth anniversary'
+  },
+  {
+    id: 2,
+    img: '/img/news/news2.png',
+    date: '',
+    text: 'A1esports Shares new picture'
+  },
+  {
+    id: 3,
+    img: '/img/news/news3.png',
+    date: '',
+    text: 'T1 unveil partnership with Razer'
+  },
+  {
+    id: 4,
+    img: '/img/news/news4.png',
+    date: '',
+    text: 'RX secures content partnership with'
+  }
+]
